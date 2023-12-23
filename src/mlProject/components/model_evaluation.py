268b2ef338c206ae6d9b98321676ad9ddd9b5417
vishwa <saivishwa.gaddam.com>
2023-12-22 22:59:10 -0800

    Final Code

--- src/mlProject/components/model_evaluation.py
+++ src/mlProject/components/model_evaluation.py
@@ -23,8 +23,7 @@ class ModelEvaluation:
         return rmse, mae, r2
     
 
-
-    def log_into_mlflow(self):
+    def save_results(self):
 
         test_data = pd.read_csv(self.config.test_data_path)
         model = joblib.load(self.config.model_path)
@@ -32,29 +31,44 @@ class ModelEvaluation:
         test_x = test_data.drop([self.config.target_column], axis=1)
         test_y = test_data[[self.config.target_column]]
         
-        mlflow.set_registry_uri(self.config.mlflow_uri)
-        tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+        predicted_qualities = model.predict(test_x)
 
-        with mlflow.start_run():
-                   predicted_qualities = model.predict(test_x)
-
-                   (rmse, mae, r2) = self.eval_metrics(test_y, predicted_qualities)
+        (rmse, mae, r2) = self.eval_metrics(test_y, predicted_qualities)
         
-                   # Saving metrics as local
-                   scores = {"rmse": rmse, "mae": mae, "r2": r2}
-                   save_json(path=Path(self.config.metric_file_name), data=scores)
+        # Saving metrics as local
+        scores = {"rmse": rmse, "mae": mae, "r2": r2}
+        save_json(path=Path(self.config.metric_file_name), data=scores)
+    # def log_into_mlflow(self):
 
-                   mlflow.log_params(self.config.all_params)
-                   mlflow.log_metric("rmse",rmse)
-                   mlflow.log_metric("r2",r2)
-                   mlflow.log_metric("mae",mae)
+    #     test_data = pd.read_csv(self.config.test_data_path)
+    #     model = joblib.load(self.config.model_path)
+
+    #     test_x = test_data.drop([self.config.target_column], axis=1)
+    #     test_y = test_data[[self.config.target_column]]
+        
+    #     mlflow.set_registry_uri(self.config.mlflow_uri)
+    #     tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+
+    #     with mlflow.start_run():
+    #                predicted_qualities = model.predict(test_x)
+
+    #                (rmse, mae, r2) = self.eval_metrics(test_y, predicted_qualities)
+        
+    #                # Saving metrics as local
+    #                scores = {"rmse": rmse, "mae": mae, "r2": r2}
+    #                save_json(path=Path(self.config.metric_file_name), data=scores)
+
+    #                mlflow.log_params(self.config.all_params)
+    #                mlflow.log_metric("rmse",rmse)
+    #                mlflow.log_metric("r2",r2)
+    #                mlflow.log_metric("mae",mae)
                    
-                   if tracking_url_type_store != "file":
+    #                if tracking_url_type_store != "file":
 
-                # Register the model
-                # There are other ways to use the Model Registry, which depends on the use case,
-                # please refer to the doc for more information:
-                # https://mlflow.org/docs/latest/model-registry.html#api-workflow
-                       mlflow.sklearn.log_model(model, "model", registered_model_name="ElasticnetModel")
-                   else:
-                        mlflow.sklearn.log_model(model, "model")
+    #             # Register the model
+    #             # There are other ways to use the Model Registry, which depends on the use case,
+    #             # please refer to the doc for more information:
+    #             # https://mlflow.org/docs/latest/model-registry.html#api-workflow
+    #                    mlflow.sklearn.log_model(model, "model", registered_model_name="ElasticnetModel")
+    #                else:
+    #                     mlflow.sklearn.log_model(model, "model")
--- src/mlProject/pipeline/stage_05_model_evaluation.py
+++ src/mlProject/pipeline/stage_05_model_evaluation.py
@@ -13,7 +13,8 @@ class ModelEvaluationTrainingPipeline:
         config = ConfigurationManager()
         model_evaluation_config = config.get_model_evaluation_config()
         model_evaluation_config = ModelEvaluation(config=model_evaluation_config)
-        model_evaluation_config.log_into_mlflow()
+        #model_evaluation_config.log_into_mlflow()
+        model_evaluation_config.save_results()
 
 
 
