df548493249108238128c4f1c06bd1d7e658e568
vishwa <saivishwa.gaddam.com>
2023-12-22 16:47:42 -0800

    Model Evaluation with mlflow

--- README.md
+++ README.md
@@ -9,3 +9,20 @@ update the components
 update the pipeline
 update the main.py
 update the app.py
+
+
+
+MLFLOW_TRACKING_URI=https://dagshub.com/saivishwa.gaddam/End_to_End_Wine_Quality_project.mlflow \
+MLFLOW_TRACKING_USERNAME=saivishwa.gaddam \
+MLFLOW_TRACKING_PASSWORD=c42e07064b62f39d7b3a7d6418104507193f8007 \
+python script.py
+
+$env:MLFLOW_TRACKING_URI="https://dagshub.com/saivishwa.gaddam/End_to_End_Wine_Quality_project.mlflow"
+
+$env:MLFLOW_TRACKING_USERNAME="saivishwa.gaddam" 
+
+$env:MLFLOW_TRACKING_PASSWORD="c42e07064b62f39d7b3a7d6418104507193f8007"
+
+os.environ["MLFLOW_TRACKING"]= "https://dagshub.com/saivishwa.gaddam/End_to_End_Wine_Quality_project.mlflow"
+os.environ["MLFLOW_TRACKING_USERNAME"] = "saivishwa.gaddam" 
+os.environ["MLFLOW_TRACKING_PASSWORD"] = "c42e07064b62f39d7b3a7d6418104507193f8007"
--- src/mlProject/config/configuration.py
+++ src/mlProject/config/configuration.py
@@ -1,7 +1,7 @@
 from mlProject.constants import *
 from mlProject.utils.common import read_yaml, create_directories
 
-from  mlProject.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig,ModelTrainerConfig
+from  mlProject.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig,ModelTrainerConfig,ModelEvaluationConfig
 
 
 class ConfigurationManager:
@@ -78,3 +78,24 @@ class ConfigurationManager:
         )
 
         return model_trainer_config
+    
+    
+    def get_model_evaluation_config(self) -> ModelEvaluationConfig:
+        config = self.config.model_evaluation
+        params = self.params.ElasticNet
+        schema =  self.schema.TARGET_COLUMN
+
+        create_directories([config.root_dir])
+
+        model_evaluation_config = ModelEvaluationConfig(
+            root_dir=config.root_dir,
+            test_data_path=config.test_data_path,
+            model_path = config.model_path,
+            all_params=params,
+            metric_file_name = config.metric_file_name,
+            target_column = schema.name,
+            mlflow_uri = "https://dagshub.com/saivishwa.gaddam/End_to_End_Wine_Quality_project.mlflow"
+           
+        )
+
+        return model_evaluation_config
